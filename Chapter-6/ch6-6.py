f27693717af6eeaf86253b35c9880559fc8148bb
aladinbd <md.alauddin19@gmail.com>
2020-01-19 07:43:49 +0600

    Initial Commit

---
+++ Chapter-6/ch6-6.py
@@ -0,0 +1,6 @@
+# Using get() method to access the value
+alien_0 = {'color': 'green', 'speed': 'slow'}
+print(alien_0)
+
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
